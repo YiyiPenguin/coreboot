e4256b75d0a0d8df7e709d9e4c897a2fb5db6b50
Matt DeVillier <matt.devillier@gmail.com>
2025-03-17 18:56:48 -0500

    soc/intel/common: Hook up CFR options for ME state/counter
    The ME state and counter option variables are already plumbed into
    the CSE code, so make them available via a common header to any
    mainboards which may want to expose the settings via CFR.
    
    Change-Id: I97d5d8b78cc9e5516dbfc64f81a925b1715b941b
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

---
+++ src/soc/intel/common/block/include/intelblocks/cfr.h
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+/*
+ * CFR enums and structs which are used to control various Intel common block settings.
+ */
+
+ #include <drivers/option/cfr_frontend.h>
+
+
+/* Intel ME State */
+static const struct sm_object me_state = SM_DECLARE_ENUM({
+	.opt_name	= "me_state",
+	.ui_name	= "Intel Management Engine",
+	.ui_helptext	= "Enable or disable the Intel Management Engine",
+	.default_value	= 1,
+	.values		= (const struct sm_enum_value[]) {
+				{ "Disabled",		1		},
+				{ "Enabled",		0		},
+				SM_ENUM_VALUE_END			},
+});
+
+/* Intel ME State Counter */
+static const struct sm_object me_state_counter = SM_DECLARE_NUMBER({
+	.opt_name	= "me_state_counter",
+	.ui_name	= "ME State Counter",
+	.flags		= CFR_OPTFLAG_SUPPRESS,
+	.default_value	= 0,
+});
